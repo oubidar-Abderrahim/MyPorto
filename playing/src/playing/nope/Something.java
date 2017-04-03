dd4f6ed8a7c40e585340af7c085b9123856aabc7
OUBIDAR Abderrahim <oubidar.abderrahim@gmail.com>
2017-04-03 03:56:01 +0100

    just playing around!

---
+++ playing/src/playing/nope/Something.java
@@ -0,0 +1,36 @@
+package playing.nope;
+
+
+
+public class Something {
+
+	private String name;
+	private int age;
+	
+	public Something() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Something [name=" + name + ", age=" + age + "]";
+	}
+	
+	
+	
+	
+	
+	
+}
+
+
+
