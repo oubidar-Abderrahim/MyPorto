713393148616712efa008c9ed7b37507e64d1eda
oubidar-Abderrahim <oubidar.abderrahim@gmail.com>
2017-05-20 12:26:15 +0100

    Update CostumerController.java

--- spring-mvc-demo/src/net/satityr/springMVCdemo/mvc/CostumerController.java
+++ spring-mvc-demo/src/net/satityr/springMVCdemo/mvc/CostumerController.java
@@ -1,30 +1,56 @@
 package net.satityr.springMVCdemo.mvc;
 
+import javax.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/costumer")
 public class CostumerController {
 	
-	@RequestMapping("/showForm")
-	public String showPage(Model theModel) {
-  
-    theModel.addAttribute("costumer", new Costumer());
-		return "costumer-form";
+	// add an initbinder ... to convert trim input strings
+	// remove leading and trailing whitespace
+	// resolve issue for validation
+	
+	@InitBinder
+	public void initBinder(WebDataBinder dataBinder) {
 		
+		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+		
+		dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+	}
+	
+	
+	@RequestMapping("/showForm")
+	public String showForm(Model theModel) {
+		
+		theModel.addAttribute("customer", new Customer());
+		
+		return "customer-form";
 	}
   
   // using spring & hibernate validator
   
-  @RequestMapping("/processForm")
-	public String processIt(@ModelAttribute("student") Student student) {
-
-    // more code coming soon
-
-		return "student-confirmation";
+	@RequestMapping("/processForm")
+	public String processForm(
+			@Valid @ModelAttribute("customer") Customer theCustomer,
+			BindingResult theBindingResult) {
+		
+		//debugging message
+		System.out.println("Last name: |" + theCustomer.getLastName() + "|");
+		
+		if (theBindingResult.hasErrors()) {
+			return "customer-form";
+		}
+		else {
+			return "customer-confirmation";
+		}
 	}
 
 }
