907356ed9cabd39b15ae26d1c7227ed0c944ceac
oubidar-Abderrahim <oubidar.abderrahim@gmail.com>
2017-06-21 05:47:52 +0000

    Update UserService.java

--- ServeratWSJRS/src/com/myServer/Services/UserService.java
+++ ServeratWSJRS/src/com/myServer/Services/UserService.java
@@ -2,12 +2,14 @@ package com.myServer.Services;
 
 import java.util.List;
 
-import javax.ws.rs.Consumes;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.PostParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.core.MediaType;
 
 import com.myServer.Business.UserBusiness;
@@ -46,6 +48,7 @@ public class UserService {
 	@POST
 	@Path("/signup")
 	@Consumes(MediaType.APPLICATION_JSON+"; charset=utf-8")
+	@Produces(MediaType.APPLICATION_JSON+"; charset=utf-8")
 	public Message signUp(User newUser){
 		
 		return userBusiness.signUp(newUser);
@@ -53,8 +56,9 @@ public class UserService {
 	
 	@POST
 	@Path("/login")
-	@Consumes(MediaType.APPLICATION_JSON+"; charset=utf-8")
-	public Message logIn(String userName, String password){
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED) // A String constant representing ""application/x-www-form-urlencoded"" media type.
+	@Produces(MediaType.APPLICATION_JSON+"; charset=utf-8")
+	public Message logIn(@PostParam (value="userName")String userName, @PostParam (value="password")String password){
 		
 		return userBusiness.logIn(userName, password);
 	}	
