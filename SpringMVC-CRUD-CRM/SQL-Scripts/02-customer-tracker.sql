0a184a430001f5c43de1fdf2a2257b0e1fb9a288
Abderrahim <Abderrahim@ABDERRAHIM-SKYLADS>
2017-08-09 18:19:39 +0100

    add OneToOne relational mapping and Date support - part1

--- SpringMVC-CRUD-CRM/SQL-Scripts/02-customer-tracker.sql
+++ SpringMVC-CRUD-CRM/SQL-Scripts/02-customer-tracker.sql
@@ -1,10 +1,10 @@
-CREATE DATABASE  IF NOT EXISTS `web_customer_tracker` /*!40100 DEFAULT CHARACTER SET latin1 */;
+CREATE DATABASE  IF NOT EXISTS `web_customer_tracker` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `web_customer_tracker`;
 -- MySQL dump 10.13  Distrib 5.6.13, for osx10.6 (i386)
 --
 -- Host: 127.0.0.1    Database: web_customer_tracker
 -- ------------------------------------------------------
--- Server version	5.6.16
+-- Server version   5.6.16
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -16,6 +16,8 @@ USE `web_customer_tracker`;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+SET FOREIGN_KEY_CHECKS = 0;
 
 --
 -- Table structure for table `customer`
@@ -30,23 +32,46 @@ CREATE TABLE `customer` (
   `last_name` varchar(45) DEFAULT NULL,
   `email` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `customer`
 --
 
+--
+-- Table structure for table `order_business`
+--
+
+DROP TABLE IF EXISTS `order_business`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `order_business` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `description` varchar(45) DEFAULT NULL,
+  `date_of_purchase` DATETIME DEFAULT NULL,
+  `customer_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_DETAIL_idx` (`customer_id`),
+  CONSTRAINT `FK_DETAIL` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+
 LOCK TABLES `customer` WRITE;
 /*!40000 ALTER TABLE `customer` DISABLE KEYS */;
 
 INSERT INTO `customer` VALUES 
-	(1,'David','Adams','david@luv2code.com'),
-	(2,'John','Doe','john@luv2code.com'),
-	(3,'Ajay','Rao','ajay@luv2code.com'),
-	(4,'Mary','Public','mary@luv2code.com'),
-	(5,'Maxwell','Dixon','max@luv2code.com');
+    (1,'David','Adams','david@luv2code.com'),
+    (2,'John','Doe','john@luv2code.com'),
+    (3,'Ajay','Rao','ajay@luv2code.com'),
+    (4,'Mary','Public','mary@luv2code.com'),
+    (5,'Maxwell','Dixon','max@luv2code.com');
 
+
+    
+    
 /*!40000 ALTER TABLE `customer` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -58,5 +83,7 @@ UNLOCK TABLES;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+SET FOREIGN_KEY_CHECKS = 1;
 
 -- Dump completed on 2016-09-24 21:50:59
---
+++ SpringMVC-CRUD-CRM/src/com/luv2code/springdemo/entity/OrderBusiness.java
@@ -0,0 +1,89 @@
+package com.luv2code.springdemo.entity;
+
+import java.util.Date;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+
+@Entity
+@Table(name="order_business")
+public class OrderBusiness {
+  
+  
+  @Id
+  @GeneratedValue(strategy=GenerationType.IDENTITY)
+  @Column(name="id")
+  private int id;
+  
+  
+  @Column(name="description")
+  private String description;
+  
+  @Column(name="date_of_purchase")
+  @Temporal(TemporalType.DATE)
+  private Date dateOfPurchase ;
+  
+  @OneToOne(cascade=CascadeType.ALL)
+  @JoinColumn(name="customer_id")
+  private Customer customer;
+
+  public OrderBusiness() {
+    
+  }
+
+  public OrderBusiness(String description, Date dateOfPurchase) {
+    this.description = description;
+    this.dateOfPurchase = dateOfPurchase;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public Date getDateOfPurchase() {
+    return dateOfPurchase;
+  }
+
+  public void setDateOfPurchase(Date dateOfPurchase) {
+    this.dateOfPurchase = dateOfPurchase;
+  }
+
+  public Customer getCustomer() {
+    return customer;
+  }
+
+  public void setCustomer(Customer customer) {
+    this.customer = customer;
+  }
+
+  @Override
+  public String toString() {
+    return "OrderBusiness [id=" + id + ", description=" + description + ", dateOfPurchase="
+        + DateUtils.formatDate(dateOfPurchase) + ", customer=" + customer + "]";
+  }
+  
+  
+
+}
